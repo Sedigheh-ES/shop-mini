9dcae7b36f0301313f2df9269020a7defa08c526
Sedi <sedi@gmail.com>
2025-10-20 18:50:33 +0330

    cart badge added

--- src/components/CartBtn/Cartbtn.tsx
+++ src/components/CartBtn/Cartbtn.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { TotalPriceSelector, totalCartItemsSelector } from '@/pages/store/cartSlice';
+import { useAppSelector } from '@/pages/store/store';
+import { ShoppingCartIcon } from '@heroicons/react/16/solid';
+import { relative } from 'path';
 
-export default function Cartbtn() {
+
+interface Props{
+    className?: string;
+}
+
+export default function Cartbtn(props: Props) {
+    const totalItems = useAppSelector(totalCartItemsSelector);
+
   return (
-    <div>Cartbtn</div>
+      <div className={`${props.className} relative`}> 
+          <ShoppingCartIcon className='w-12 h-12' />
+          {!!totalItems &&
+              <div className='bg-red-500 text-white w-8 h-8 rounded-full text-center flex justify-center items-center absolute -top-3 -right-3'>{totalItems} </div>}
+          
+      </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,17 @@
+import {  ShoppingCartIcon } from '@heroicons/react/16/solid'
 import React from 'react'
+import Cartbtn from '../CartBtn/Cartbtn'
+import Link from 'next/link'
 
 export default function () {
   return (
-      <div className='flex justify-between items-center border py-4 px-4'>
-          <div>logo</div>
-
-          <div className='flex justify-between items-center gap-2 py-2'>
-              <div>menu</div>
-              <div>search</div>
-              <div>cart</div>
-              <div>user</div>
-          </div>
-    </div>
+      <header className="bg-gradient-to-b from-sky-200 to-sky-50 border shadow p-8 flex">
+      <Link className="text-sky-600" href={"/"}>
+        Home
+      </Link>
+      <Link className="ml-auto mr-4" href={"/cart"}>
+        <Cartbtn />
+      </Link>
+    </header>
   )
 }
