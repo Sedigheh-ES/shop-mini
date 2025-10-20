851e3fcc9ec1e44e94f7f29bf9d815ff187cb8ef
Sedi <sedi@gmail.com>
2025-10-20 18:23:18 +0330

    inc/dec btn

--- src/components/QtyBtn/QtyBtn.tsx
+++ src/components/QtyBtn/QtyBtn.tsx
@@ -1,3 +1,4 @@
+import { TrashIcon } from '@heroicons/react/16/solid';
 import React from 'react';
 
 interface Props{
@@ -9,7 +10,10 @@ interface Props{
 export default function QtyBtn(props:Props) {
   return (
       <div className='flex gap-2 justify-center items-center'>
-          <button className='px-4 py-2 bg-red-500 text-white rounded-md' onClick={props.onDecrease}>-</button>
+          <button className='px-4 py-2 bg-red-500 text-white rounded-md' onClick={props.onDecrease}>
+              {props.qty === 1 ?<TrashIcon className='w-3 h-6'/>:"-"}
+             
+          </button>
           <p>{props.qty}</p>
           <button className='px-4 py-2 bg-sky-500 text-white rounded-md' onClick={props.onIcrease}>+</button>
           
