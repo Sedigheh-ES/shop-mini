c07009b41542473392e64e13dbec484afaa1d51a
Sedi <sedi@gmail.com>
2025-10-20 13:25:21 +0330

    product  Card commit

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Product } from '@/types/interfaces'
+import Image from 'next/image'
 import React from 'react'
 
 interface Props{
@@ -7,6 +8,20 @@ interface Props{
 
 export default function ProductCard(props:Props) {
   return (
-      <div className='border'>{props.product.name}</div>
+      <div className='border rounded-lg transition shadow overflow-hidden gap-3'>
+          <Image
+              src={props.product.imagePath}
+              width={300} height={400}
+              alt={props.product.name} >
+              
+              </Image>
+         
+          <div className='p-2'>
+              <h6 className='text-center text-slate-600'>{props.product.name}</h6>
+              <p className='text-center text-slate-600'>{props.product.price}$ </p>
+              
+          </div>
+      </div>
+      
   )
 }
--- src/pages/mock/productData.ts
+++ src/pages/mock/productData.ts
@@ -5,24 +5,24 @@ export const prodcutData: Product[] = [
         id: 0,
         name: 'iPhone11',
         price: 1200,
-        imagePath:"/image/iPhone-1.ipg"
+        imagePath:"/image/pro-1.jpg"
     },
      {
         id: 1,
         name: 'iPhone12',
         price: 1500,
-        imagePath:"/image/iPhone-2.ipg"
+        imagePath:"/image/pro-2.jpg"
     },
       {
         id: 2,
         name: 'iPhone13',
         price: 3500,
-        imagePath:"/image/iPhone-3.ipg"
+        imagePath:"/image/pro-3.jpg"
     },
        {
         id: 3,
         name: 'iPhone14',
         price: 2200,
-        imagePath:"/image/iPhone-4.ipg"
+        imagePath:"/image/watch-2.jpg"
     },
 ]
