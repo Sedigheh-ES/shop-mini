c6b3be00c4e7d197f2c321419285a54b82ae49e9
Sedi <sedi@gmail.com>
2025-10-20 11:57:08 +0330

    cart setting

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function () {
+  return (
+      <div className='flex justify-between items-center border py-4 px-4'>
+          <div>logo</div>
+
+          <div className='flex justify-between items-center gap-2 py-2'>
+              <div>menu</div>
+              <div>search</div>
+              <div>cart</div>
+              <div>user</div>
+          </div>
+    </div>
+  )
+}
